c6adbf636a24cf6e3264071ec210b7a2d1c7ab6f
Abraham <ricoabraham879@gmail.com>
2025-09-23 21:41:05 -0500

    Estructura del proyecto - Standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./shared/footer/footer.component";
+import { HeaderComponent } from "./shared/header/header.component";
+
+declare var $: any;
+declare function HOMEINIT([]): any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecommerce';
+
+  constructor() {
+    setTimeout(() => {
+      HOMEINIT($);
+    }, 50);
+
+    $(window).on('load', function () {
+      $("#loading").fadeOut(500);
+    });
+  }
 }
