1153aca6bc324caad8ab14f79837e01f4a6b55af
Abraham <ricoabraham879@gmail.com>
2025-09-28 11:21:07 -0500

    Registro funcional de un nuevo usuario

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  name: string = '';
+  surname: string = '';
+  email: string = '';
+  phone: string = '';
+  password: string = '';
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
+
+  register() {
+    if (!this.name || !this.surname || !this.email || !this.phone || !this.password) {
+      this.toastr.error('Por favor, completa todos los campos.', 'Error de Registro');
+      return;
+    }
+
+    let data = {
+      name: this.name,
+      surname: this.surname,
+      email: this.email,
+      phone: this.phone,
+      password: this.password
+    };
+
+    this.authService.register(data).subscribe((resp: any) => {
+      this.toastr.success('Revisa tu correo electrónico para completar el registro.', 'Éxito');
+      setTimeout(() => {
+        this.router.navigateByUrl("/login");
+      }, 500);
+    });
+  }
 }
